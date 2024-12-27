b0630b9b0085566fca0cbaa666ad8ab2f47833d5
Ducky Gnome <anirealmsevil@gmail.com>
2024-12-27 15:55:49 -0600

    changes for getting the expand panel to open and close as well as fixing soem formating issues

--- todo_calendar/classes/todo_list_item.gd
+++ todo_calendar/classes/todo_list_item.gd
@@ -2,14 +2,22 @@ extends VBoxContainer
 
 @onready var todo_description: Label = %todo_description
 @export var todo_description_text: String = ""
+@onready var expanded_details: MarginContainer = %expanded_details
+@onready var task_description_input: TextEdit = %task_description_input
+@onready var state_check: CheckBox = %state_check
+@onready var reoccurring_check: CheckBox = %reoccurring_check
+@onready var task_name_input: TextEdit = %task_name_input
 
 signal delete_todo
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	expanded_details.visible = false
 	todo_description.text = todo_description_text
-
-
-
+	task_name_input.text = todo_description_text
+	
 func _on_delete_button_pressed() -> void:
 	delete_todo.emit(self)
+
+func _on_expand_button_pressed() -> void:
+	expanded_details.visible = !expanded_details.visible
